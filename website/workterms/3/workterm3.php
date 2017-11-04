c1dcecebc41ac90bdecb377d9f9606a7bdf0432b
nickolas leipold <nickolas.leipold@hotmail.com>
2017-11-03 22:57:57 -0400

    changed file structure as well as nginx config to have only certain places whitelisted, working on changing the default style for bootstrap

---
+++ website/workterms/3/workterm3.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Workterm Report 3</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/style.page.css">
+  <script src="js/jquery-3.2.1.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">nickolasLeipold</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="../1/workterm1.php">workterm1</a></li>
+      <li><a href="../2/workterm2.php">workterm2</a></li>
+      <li><a href="../3/workterm3.php">workterm3</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="jumbotron text-center">
+    <h1>Workterm Report 3</h1>
+    <br>
+    <h3>By:Nickolas Leipold</h3>
+</div>
+  
---
+++ website/workterms/index.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Workterm Report</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.page.css">
+  <script src="js/jquery-3.2.1.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">nickolasLeipold</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="1/workterm1.php">workterm1</a></li>
+      <li><a href="2/workterm2.php">workterm2</a></li>
+      <li><a href="3/workterm3.php">workterm3</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="jumbotron text-center">
+    <h1>Workterm report</h1>
+    <br>
+    <h3>By:Nickolas Leipold</h3>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
+<?php
+
+echo "
+        <h1>this is the start of the website i have to make for co-op assignment</h1>
+        <br>
+    ";
+
+echo "hello world!";
