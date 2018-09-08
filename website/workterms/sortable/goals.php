43e8a39df0b5f17a70c71e3ca216322e0230bc1b
nickWinDesktop <nleipold@uoguelph.ca>
2018-09-07 23:08:03 -0400

    Sortable Goals Complete

--- website/workterms/sortable/goals.php
+++ website/workterms/sortable/goals.php
@@ -25,21 +25,31 @@ echo file_get_contents(__DIR__ . "/../shared/nav_bar.html");
 
 <div id="goals">
     <div>
-        <h4>I would like to become more proficient in functional coding</h4>
+        <h4>Become more proficient in functional programming</h4>
+        <p>&nbsp;&nbsp;From day one at Sortable I was ushered into the world of functional programing which was something I had never really noticed before.
+        I knew very little about it other than functions aren't always executed when they are called. The language we use is called Scala which runs on the JVM
+        Through several tasks and PR's coming back with comments such as do this in a 'more Scala way', I learned what was meant and I really started to enjoy the nuances
+        of the language and the coding style.</p>
     </div>
     <br>
     <div>
-        <h4>I would like to complete tasks with a higher degree of clarity and understanding of functionality with the
-            languages i work with</h4>
+        <h4>Execute tasks with a higher degree of clarity and understanding</h4>
+        <p>&nbsp;&nbsp;This is always a challenge no matter where you work or who you work with. This was a goal to be worked on so I could be more productive and make a more concise solution to the current problem, and not the perceived one.
+        I believe i made great progress on this goal because i was later interfacing with other co-workers and gathering the details of what they actually want completed and was able to think and create a solution much more quickly.
+        </p>
     </div>
     <br>
     <div>
-        <h4>I would like to become more clear at conveying thoughts and ideas and code changes</h4>
+        <h4>Proficient at conveying thoughts and ideas</h4>
+        <p>&nbsp;&nbsp;Conveying thoughts about how new systems should function and how problems should be solved was a goal I had for this workterm,
+        Clearly defining my thoughts with the correct and detailed words was worked on and my growth in the ad tech knowledge has helped with conveying my ideas correctly.
+        Ultimately helping with my comprehension of the ad tech field.</p>
     </div>
     <br>
     <div>
-        <h4>I would like to retain spoken instruction more readily and be able to write down that information for future
-            use</h4>
+        <h4>Retain spoken instruction more readily and be able to write down that information for future use</h4>
+        <p>&nbsp;&nbsp;Ideally instructions for tasks should be in a ticket which was something worked on, but none the less I have gotten better and consulting with the ticket creator and gathering the needed information to complete what is desired,
+            this is also due to getting better at abbreviating and more streamlined notes.</p>
     </div>
     <br>
 </div>
