e5afc26ea29c3a71a91cb160484fc6d237821f4e
nickWinDesktop <nleipold@uoguelph.ca>
2018-09-10 15:56:48 -0400

    conclusion spelling tweaks

--- website/workterms/sortable/workterm3.php
+++ website/workterms/sortable/workterm3.php
@@ -20,12 +20,6 @@ echo file_get_contents(__DIR__ . "/../shared/nav_bar.html");
     </div>
 
     <h2 style="text-align:center; background-color: rgba(255,255,255,0.7);"><?php echo $title ?></h2>
-
-    <style>
-        #job-description {
-            background-color: rgba(255, 255, 255, 0.7);
-        }
-    </style>
     <div id="job-description">
         <h2 style="text-align:center;">Job Description</h2>
         <img id="group-picture" src="SortableTeamPhoto2018.jpg">
@@ -37,15 +31,15 @@ echo file_get_contents(__DIR__ . "/../shared/nav_bar.html");
         <div id="job-desc">
             My job as a Software Developer Co-op involved working a new product that was to be released.
             <br>
-            The project involved changes to user facing tools as well as backend and server side changes to the system itself for optimal performance.
-            Languages i got to work with for the UI tools consisted of HTML, vanilla js and some AngualrJs. Angular is a fun one, especially on a slightly older version of the language, lots of magic to deal with and files to track through,
+            The project involved making changes to user facing tools as well as backend and server side changes to the back-end itself for optimal performance.
+            Languages I used while working on UI tools consisted of HTML, vanilla js and some AngualrJs. Angular is a fun one, especially on a slightly older version of the language, lots of magic to deal with and files to track through,
             although it was a nice refresher into the world of user design and making items nice and pretty. UI changes were something that was sprinkled through my time at Sortable and I was number 2 on the team for tackling them.
             <br>
-            The majority of my work was feature enhancement to the existing back-end system, and working on getting the product to market. This is where i got to learn and work with scala,
+            The majority of my work was feature enhancement to the existing back-end system, and working on getting the product to market. On the back-end is where I got to learn and work with scala,
             by helping with prebid adapter protocol support and had the experience of sifting through objects and functions to find when a function is actually called. Functional programming showed me a new way to look at problems
             and how a problem can be solved.
             <br>
-            I found functional programming to be an eye opener as well as Scala as a language, which will help me keep a more open mind when completing other tasks and problems in my future endeavours.
+            I found functional programming to be an eye opener along with Scala as a language, which will help me keep a more open mind when completing other tasks and problems in my future endeavours.
         </div>
     </div>
 
